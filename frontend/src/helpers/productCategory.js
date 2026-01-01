ece0901c315aec300fd5d9007f70c6a8a5d44aed
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2026-01-01 23:18:36 +0530

    fix issues

--- frontend/src/helpers/productCategory.js
+++
@@ -1,8 +0,0 @@
-const productCategory = [
-    {id : 1 ,label : "Watches",value : "watches"},
-    {id : 2 ,label : "Shoes",value : "shoes"},
-    {id : 3 ,label : "Shirts",value : "shirts"},
-    {id : 4 ,label : "Shorts",value : "shorts"},
-]
-
-export default productCategory
